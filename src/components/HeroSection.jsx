296cc26ae07d8d6c1a669c3d230327542f3cb24b
Soubir Saian <soubir2018@gmail.com>
2025-06-01 22:24:34 +0600

    hero-updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,13 +6,13 @@ const HeroSection = () => {
   return (
     <div className=' text-white bg-[url("/BG-blue.png")] bg-no-repeat bg-center bg-cover py-16 mb-[50px]'>
 
-      <div className=' pt-24'>
+      <div className='container mx-auto pt-24'>
 
         <div className='grid grid-cols-1 md:grid-cols-2 place-items-center'>
 
           <div className='max-w-[90%] mr-auto'>
-            <h1 className='text-5xl font-bold'>Make your Saas and Awesome Design</h1>
-            <p className='text-sm opacity-70 mt-6'>It is a long established fact  that a reader will be distracted by the readable content of a page when looking</p>
+            <h1 className='text-5xl text-wrap font-bold'>Make your Saas and Awesome Design</h1>
+            <p className='text-sm text-wrap opacity-70 mt-6'>It is a long established fact  that a reader will be distracted by the readable content of a page when looking</p>
             <div className='flex gap-6 mt-6'>
                 <button className="bg-white text-gray-700  text-sm hover:bg-black hover:text-white active:scale-95 transition-all duration-400 w-40 h-11 rounded-full">Contact us</button>
                 <button className="bg-black text-white text-sm hover:bg-white hover:text-black active:scale-95 transition-all duration-400 w-40 h-11 rounded-full">Get started</button>
